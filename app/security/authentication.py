8adfac1e65d8d00489f367fe2bd777c2b3f3f820
Kasula-Lohith-Kumar <kasulalohithkumar@outlook.com>
2025-11-18 21:32:40 +0530

    fix

--- app/security/authentication.py
+++ app/security/authentication.py
@@ -177,7 +177,7 @@ def is_streamlit_cloud() -> bool:
     """Detect Streamlit Cloud by checking if st.secrets has Streamlit-managed credentials."""
     print('GCP Service Account Check ...')
     try:
-        _ = st.Secrets["gcp_service_account"]
+        _ = st.secrets["gcp_service_account"]
         return True
     except Exception:
         return False
