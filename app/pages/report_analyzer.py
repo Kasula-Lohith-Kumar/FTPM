1ed9a1d70bbe0390846dc4dd4b319230d41ae0b3
Kasula-Lohith-Kumar <kasulalohithkumar@outlook.com>
2025-11-20 17:20:02 +0530

    fix

--- app/pages/report_analyzer.py
+++ app/pages/report_analyzer.py
@@ -126,6 +126,7 @@ def run():
         if user_input:
             # Placeholder chatbot response (replace with your model logic)
             response = lap.text_retraivalQA(st.session_state.chroma_database, user_input).content
+            dp.display_content(response)
             # response = f"🤖 (Mock Response) The analysis for '{user_input}' will appear here."
             st.session_state.chat_history.append((user_input, response))
 
